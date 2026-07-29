14a60a0ad599be1339f58bde2e1d1d694f98a31b
shayan368 <shayanahmad368@gmail.com>
2026-07-29 10:58:57 +0500

    added pollroutes and userroutes

---
+++ routes/pollRoutes.js
@@ -0,0 +1,34 @@
+import express from "express";
+import { upload } from "../config/cloudinary.js";
+import { protect, optionalAuth } from "../middleware/auth.js";
+import {
+  createPoll,
+  getPolls,
+  getPollById,
+  votePoll,
+  closePoll,
+  deletePoll,
+  getMyVotedPolls,
+  getPollTypeCounts,
+} from "../controllers/pollController.js";
+import { addComment, getComments } from "../controllers/commentController.js";
+
+const router = express.Router();
+
+router.post("/", protect, upload.array("images", 6), createPoll);
+router.get("/", optionalAuth, getPolls);
+
+
+router.get("/mine/voted", protect, getMyVotedPolls);
+router.get("/types/counts", getPollTypeCounts);
+
+router.get("/:id", optionalAuth, getPollById);
+router.post("/:id/vote", protect, votePoll);
+router.put("/:id/close", protect, closePoll);
+router.delete("/:id", protect, deletePoll);
+
+// comments 
+router.post("/:pollId/comments", protect, addComment);
+router.get("/:pollId/comments", getComments);
+
+export default router;
---
+++ routes/userRoutes.js
@@ -0,0 +1,28 @@
+import express from "express";
+import { upload } from "../config/cloudinary.js";
+import { protect, optionalAuth } from "../middleware/auth.js";
+import {
+  getMe,
+  getMyBookmarks,
+  getUserByUsername,
+  updateProfile,
+  changePassword,
+  deleteAccount,
+  followUser,
+  toggleBookmark,
+} from "../controllers/userController.js";
+
+const router = express.Router();
+
+router.get("/me", protect, getMe);
+router.get("/me/bookmarks", protect, getMyBookmarks);
+router.put("/profile", protect, upload.single("avatar"), updateProfile);
+router.put("/change-password", protect, changePassword);
+router.delete("/me", protect, deleteAccount);
+
+router.put("/:id/follow", protect, followUser);
+router.put("/bookmarks/:pollId", protect, toggleBookmark);
+
+router.get("/:username", optionalAuth, getUserByUsername);
+
+export default router;
